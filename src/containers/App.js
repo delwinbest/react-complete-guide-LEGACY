3a65adfd104457e97e60031da0ac7809ab8be99e
Delwin Best <mail@delwinbest.com>
2020-12-23 16:09:32 +0900

    convert functions to Classes

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,12 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: '001', name: 'Max', age: 28 },
@@ -16,6 +22,18 @@ class App extends Component {
     showPersons: false
   }
 
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps');
+    return state;
+  }
+
+  // componentWillMount(){
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount');
+  }
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
@@ -44,6 +62,7 @@ class App extends Component {
   }
 
   render () {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -58,6 +77,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons} 
           persons={this.state.persons} 
           clicked={this.togglePersonsHandler} />
