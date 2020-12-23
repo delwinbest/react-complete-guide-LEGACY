2521b16ad0993660cfa355ea016b5eb3bde5aa26
Delwin Best <mail@delwinbest.com>
2020-12-23 20:05:28 +0900

    using prop types

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
@@ -18,6 +20,13 @@ class Person extends Component {
         </Aux>
         );
     }
+};
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
