83933ea314d7065c90a5601e1955daae045db86f
Delwin Best <mail@delwinbest.com>
2020-12-23 18:46:19 +0900

    optimised component loads, no reload for cockpi

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -43,10 +43,10 @@ const cockpit = (props) => {
       btnClasses = classes.Red;
   }
   
-  if (props.persons.length <= 2) {
+  if (props.personsLength<= 2) {
     assignedClasses.push(classes.red);
   }
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold);
   }
   
@@ -62,4 +62,4 @@ const cockpit = (props) => {
   );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
