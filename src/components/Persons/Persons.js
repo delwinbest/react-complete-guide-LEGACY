d5e05bf3f962a117ca07a99f2a53e3f33e671755
Delwin Best <mail@delwinbest.com>
2020-12-23 16:31:02 +0900

    added lifecycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,6 +3,34 @@ import Person from './Person/Person';
 
 class Persons extends Component {
 //const persons = props => {
+  // static getDerivedStateFromProps(props, state){
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  // componentWillReceiveProps(props){
+  //   console.log('[Persons.js] shouldComponentUpdate', props);
+  // }
+
+  // componentWillUpdate(){
+
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!' };
+  }
+
+  componentDidUpdate(prevProps, preState, snapshot){
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
   render() {
     console.log('[Persons.js] rendering...');
     return this.props.persons.map((person, index) => {
--- src/containers/App.js
+++ src/containers/App.js
@@ -34,6 +34,16 @@ class App extends Component {
   componentDidMount(){
     console.log('[App.js] componentDidMount');
   }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
