94b536f16b06cf4c789ef5751db1801a1bd8b0cc
Delwin Best <mail@delwinbest.com>
2020-12-28 15:25:59 +0900

    nav toolbar with dynamic classnames

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.module.scss';
 import Logo from '../../../components/Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = ( props ) => {
    return (
     <header className={classes.Toolbar}>
             <div>MENU</div>
             <Logo />
-            <nav>
-                ...
-            </nav>
+            <NavigationItems />
         </header>
    )
 };
