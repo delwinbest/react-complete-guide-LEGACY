5a10d746e5971730c36fefca09f9f7b04bbb82f1
Delwin Best <mail@delwinbest.com>
2020-12-28 15:02:12 +0900

    working with images and logo

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './Toolbar.module.scss';
+import Logo from '../../../components/Logo/Logo';
+
+const toolbar = ( props ) => {
+   return (
+    <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <Logo />
+            <nav>
+                ...
+            </nav>
+        </header>
+   )
+};
+
+export default toolbar;
