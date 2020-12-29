5e0248b41e04acaaa842868bef36f5b9ec0ad204
Delwin Best <mail@delwinbest.com>
2020-12-29 15:48:32 +0900

    menu styling

--- src/components/Navigation/SideDrawer/MenuDrawer/MenuDrawer.js
+++ src/components/Navigation/SideDrawer/MenuDrawer/MenuDrawer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './MenuDrawer.module.scss';
 
 const drawer = ( props ) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.MenuDrawer} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawer;
