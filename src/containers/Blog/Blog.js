cf919ea76da951db3113a0443aeed04f66e158bc
Delwin Best <mail@delwinbest.com>
2020-12-30 10:42:53 +0900

    error handling

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -23,6 +24,10 @@ class Blog extends Component {
                     }
                 })
                 this.setState({posts: updatedPosts});
+            })
+            .catch(error => {
+                //console.log(error);
+                this.setState({error: true})
             });
 
     }
@@ -32,15 +37,17 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p> 
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
             return (
                 <Post 
                     key={post.id} 
                     title={post.title} 
                     author={post.author} 
                     clicked={() => this.postSelectedHandler(post.id)}/>
-            )
-        });
+            )});
+        }
         return (
             <div>
                 <section className="Posts">
