d4dc3e8daf8796912c89b6b928752e8258a157e7
Delwin Best <mail@delwinbest.com>
2021-01-01 15:08:35 +0900

    switch and router objects

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import FullPost from '../Blog/FullPost/FullPost';
 
 class Blog extends Component {
@@ -28,9 +28,14 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
+                {/* <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Route path="/:id" exact component={FullPost} /> */}
             </div>
         );
     }
