b37a8b9cfd4f9b4fe9d0c5d37a31d44200fc7116
Delwin Best <mail@delwinbest.com>
2021-01-02 16:02:59 +0900

    passing params two different ways

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Course extends Component {
     render () {
+        console.log(this.props);
+        console.log(this.state);
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.props.location.state.courseTitle}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
