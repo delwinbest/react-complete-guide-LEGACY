b822f53df4ec0ad10679feee0776d6139d609644
Delwin Best <mail@delwinbest.com>
2021-01-03 16:17:06 +0900

    nit

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -13,7 +13,12 @@ const order = (props) => {
     const ingredientString = ingredients.map(ig => {
         return <span 
             key={ig.name}
-            style={{textTransform: 'capitalize', display: 'inline-block', margin: '0 8px', border: '1px solid #ccc', padding: '5px'}}>{ig.name} ({ig.amount})</span>
+            style={{
+                textTransform: 'capitalize', 
+                display: 'inline-block', 
+                margin: '0 8px', 
+                border: '1px solid #ccc', 
+                padding: '5px'}} >{ig.name} ({ig.amount})</span>
     })
     return (
         <div className={classes.Order}>
