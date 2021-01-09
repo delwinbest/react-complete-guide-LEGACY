d6b5970b16ab21b35d46f4b44f759196e84b53b7
Delwin Best <mail@delwinbest.com>
2021-01-09 13:37:14 +0900

    passing arguments into reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -28,7 +28,17 @@ const reducer = (state = initialState, action) => {
         case 'STORE_RESULT':
             return {
                 ...state,
-                results: state.results.concat(state.counter)
+                results: state.results.concat({id: new Date().valueOf(), value: state.counter})
+            };
+        case 'DELETE_RESULT':
+            //const id = 2;
+            //state.results.splice(id, 1); //mutates array
+            //const newArray = [...state.results]
+            //newArray.results.splice(id, 1);
+            const newArray = state.results.filter((result) => result.id !== action.resultElementId ? true : false); //returns a new array
+            return {
+                ...state,
+                results: newArray
             };
         default:
             return state;
