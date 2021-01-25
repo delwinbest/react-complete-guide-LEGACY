2d7a4894256c771698dab919b3ce3e1bd55062b5
Delwin Best <mail@delwinbest.com>
2021-01-25 16:12:34 +0900

    basic auth container

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.module.scss';
+
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+
+
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            });
+        }
+        let form = (
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button btnType="Success">SUBMIT</Button>
+            </form>
+        );
+
+        return (
+            <div className={classes.Auth}>
+                <form>
+                    {form}
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
