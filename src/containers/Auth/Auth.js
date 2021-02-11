9608ed4c8422bba0b11bde7e779b5dfe89c71f9e
Delwin Best <mail@delwinbest.com>
2021-02-11 12:48:21 +0900

    manipulating auth redirect path using store

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -41,6 +41,12 @@ class Auth extends Component {
         },
     }
 
+    componentDidMount() {
+        if(!this.props.buildingState && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+
     checkValidity(value, rules) {
         let isValid = true;
         if (!rules) {
@@ -137,8 +143,10 @@ class Auth extends Component {
 
         let authRedirect = null;
         if(this.props.isAuthenticated) {
-            authRedirect = <Redirect to="/" ></Redirect>
+            authRedirect = <Redirect to={this.props.authRedirectPath} ></Redirect>
         }
+
+
         return (
             <div className={classes.Auth}>
                 {authRedirect}
@@ -154,13 +162,16 @@ const mapStatetoProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null,
+        buildingState: state.burgerBuilder.buildingState,
+        authRedirectPath: state.auth.authRedirectPath
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     }
 }
 
