c41363d04de8bdffa595bc638b981cd42693dd22
Delwin Best <mail@delwinbest.com>
2021-02-11 11:19:54 +0900

    update UI and route ifAuth

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,6 +3,7 @@ import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.module.scss'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import { connect } from 'react-redux';
 
 class Layout extends Component {
     state = {
@@ -18,8 +19,11 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <Toolbar toggleclosed={this.sideDrawerToggleHandler}/>
+                <Toolbar 
+                    isAuth={this.props.isAuthenticated}
+                    toggleclosed={this.sideDrawerToggleHandler}/>
                 <SideDrawer 
+                    isAuth={this.props.isAuthenticated}
                     open={this.state.showSideDrawer} 
                     toggleclosed={this.sideDrawerToggleHandler}/>        
                 <main  className={classes.Content}>
@@ -29,4 +33,11 @@ class Layout extends Component {
     )}
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+}
+
+
+export default connect(mapStateToProps)(Layout);
