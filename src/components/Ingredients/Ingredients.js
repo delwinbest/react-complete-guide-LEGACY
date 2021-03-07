3409e64e3e438bbaed35f4a50f93094d1edd8899
Delwin Best <mail@delwinbest.com>
2021-03-07 13:10:51 +0900

    removeIngredientHanlder

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -10,13 +10,18 @@ function Ingredients() {
   const addIngredientHandler = ingredient => {
     setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}])
   };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId))
+  };
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients}  onRemoveItem={() => {}}/>
+        <IngredientList ingredients={userIngredients}  onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
   );
