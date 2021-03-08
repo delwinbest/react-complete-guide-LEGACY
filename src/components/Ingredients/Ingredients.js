b47a3bb0bafb082e9b1ac64ab0c559ff4f9bcd9e
Delwin Best <mail@delwinbest.com>
2021-03-08 14:35:43 +0900

    using callback to use original function (not new re rendered)

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,25 +7,25 @@ import Search from './Search';
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json').then(response => {
-      return response.json()
-    }).then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        })
-      }
-      setUserIngredients(loadedIngredients);
-    });
-  }, []); //no array is like component did update, with array, its like component did mount
+  // useEffect(() => {
+  //   fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json').then(response => {
+  //     return response.json()
+  //   }).then(responseData => {
+  //     const loadedIngredients = [];
+  //     for (const key in responseData) {
+  //       loadedIngredients.push({
+  //         id: key,
+  //         title: responseData[key].title,
+  //         amount: responseData[key].amount
+  //       })
+  //     }
+  //     setUserIngredients(loadedIngredients);
+  //   });
+  // }, []); //no array is like component did update, with array, its like component did mount
 
-  useEffect(() => {
-    console.log('useEffect loaded with userIngredients')
-  }, [userIngredients])
+  // useEffect(() => {
+  //   console.log('useEffect loaded with userIngredients')
+  // }, [userIngredients])
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json', {
@@ -42,6 +42,10 @@ function Ingredients() {
     });
   };
 
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  },[]);
+
   const removeIngredientHandler = (ingredientId) => {
     setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId))
   };
@@ -51,7 +55,7 @@ function Ingredients() {
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         <IngredientList ingredients={userIngredients}  onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
