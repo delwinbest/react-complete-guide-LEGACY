902df1a8abc9f42ed95e2012015d97d2d4ba72f2
Delwin Best <mail@delwinbest.com>
2021-03-08 14:44:36 +0900

    use timeout to delay server call.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,32 +6,39 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState(''); 
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 
-      ? '' 
-      : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json'+query).then(response => {
-      return response.json()
-    }).then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        })
+    setTimeout(()=> {
+      if(enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length === 0 
+          ? '' 
+          : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json'+query).then(response => {
+          return response.json()
+        }).then(responseData => {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            })
+          }
+          onLoadIngredients(loadedIngredients);
+        });
       }
-      onLoadIngredients(loadedIngredients);
-    });
-  }, [enteredFilter, onLoadIngredients]); //no array is like component did update, with array, its like component did mount
+    },500)}, [enteredFilter, onLoadIngredients, inputRef]); //no array is like component did update, with array, its like component did mount
+    
 
+    
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
           <input type="text"
+            ref={inputRef}
             value={enteredFilter}
             onChange={event => {setEnteredFilter(event.target.value)}} />
         </div>
