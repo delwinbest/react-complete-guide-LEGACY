624597852519b02679571834ad8d67a29f700f58
Delwin Best <mail@delwinbest.com>
2021-03-28 16:58:11 +0900

    using useReducer hook to manager state

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,29 @@
-import React, { useState, useCallback } from 'react';
+import React, { useReducer, useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
-function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+const ingredientReducer = (currentIngredients, action) => {
+  switch(action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [
+        ...currentIngredients, 
+        action.ingredient
+      ]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Failed case in reducer');
+  }
+}
+
+const Ingredients = () => {
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -40,29 +57,33 @@ function Ingredients() {
       setIsLoading(false);
       return response.json();
     }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients, 
-        {id: responseData.name, ...ingredient}
-      ])
+      // setUserIngredients(prevIngredients => [
+      //   ...prevIngredients, 
+      //   {id: responseData.name, ...ingredient}
+      // ])
+      dispatch( { type: 'ADD', ingredient: {id: responseData.name, ...ingredient} } );
     });
   };
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    //setUserIngredients(filteredIngredients);
+    dispatch( { type: 'SET', ingredients: filteredIngredients } );
   },[]);
 
   const removeIngredientHandler = (ingredientId) => {
     setIsLoading(true);
     fetch(`https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients/${ingredientId}`, {
-      method: 'DELETE',
-      headers: { 
+      'method': 'DELETE',
+      'headers': { 
         'Content-Type': 'application/json',
+        'mode': 'no-cors',
         'Access-Control-Allow-Origin': '*'
       },
       status: '200'
     }).then(response => {
       setIsLoading(false);
-      setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId))
+      // setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId))
+      dispatch( { type: 'DELETE', id: ingredientId } );
     }).catch(error => {
       setError(error.message)
       setIsLoading(false);
