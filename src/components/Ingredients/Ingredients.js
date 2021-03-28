d28048fc213de5cf433dd2dad5642c114496b979
Delwin Best <mail@delwinbest.com>
2021-03-28 17:13:03 +0900

    combining states handler into useReducer hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useCallback } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -21,11 +21,27 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 }
 
+const httpReducer = (curHttpState, action) => {
+  switch(action.type) {
+    case 'SEND':
+      return { loading: true, error: null}
+    case 'RESPONSE':
+      return {...curHttpState, loading: false }
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage }
+    case 'CLEAR':
+      return { ...curHttpState, error: null }
+    default:
+      throw new Error('Failed case in reducer');
+  }
+}
+
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {loading: false, error: null});
   // const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
 
   // useEffect(() => {
   //   fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json').then(response => {
@@ -48,13 +64,14 @@ const Ingredients = () => {
   // }, [userIngredients])
 
   const addIngredientHandler = ingredient => {
-    setIsLoading(true);
+    //setIsLoading(true);
+    dispatchHttp({type: 'SEND'});
     fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json'}
     }).then(response => {
-      setIsLoading(false);
+      dispatchHttp({type: 'RESPONSE'});
       return response.json();
     }).then(responseData => {
       // setUserIngredients(prevIngredients => [
@@ -71,7 +88,8 @@ const Ingredients = () => {
   },[]);
 
   const removeIngredientHandler = (ingredientId) => {
-    setIsLoading(true);
+    //setIsLoading(true);
+    dispatchHttp({type: 'SEND'});
     fetch(`https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients/${ingredientId}`, {
       'method': 'DELETE',
       'headers': { 
@@ -81,24 +99,27 @@ const Ingredients = () => {
       },
       status: '200'
     }).then(response => {
-      setIsLoading(false);
+      //setIsLoading(false);
+      dispatchHttp({type: 'RESPONSE'});
       // setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientId))
       dispatch( { type: 'DELETE', id: ingredientId } );
     }).catch(error => {
-      setError(error.message)
-      setIsLoading(false);
+      // setError(error.message)
+      // setIsLoading(false); 
+      dispatchHttp({type: 'ERROR', errorMessage: error.message });
     });
   };
 
   const clearError = () => {
-    setError(null);
+    //setError(null);
+    dispatchHttp({type: 'CLEAR'});
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError} >{error}</ErrorModal>}
+      {httpState.error && <ErrorModal onClose={clearError} >{httpState.error}</ErrorModal>}
 
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
