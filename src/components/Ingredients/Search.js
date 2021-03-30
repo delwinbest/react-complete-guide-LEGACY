17deb8498ca91520cc95e62ac1d2d1fac42afbf6
Delwin Best <mail@delwinbest.com>
2021-03-30 10:09:01 +0900

    move search to useEffect

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import useHttp from '../../hooks/http';
+import ErrorModal from '../UI/ErrorModal';
+
 import './Search.css';
 
 const Search = React.memo(props => {
@@ -8,37 +11,46 @@ const Search = React.memo(props => {
   const [enteredFilter, setEnteredFilter] = useState(''); 
   const inputRef = useRef();
 
+  const {isLoading, data, error, sendRequest, clear} = useHttp();
+
   useEffect(() => {
     const timer = setTimeout(()=> {
       if(enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length === 0 
           ? '' 
           : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch('https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json'+query).then(response => {
-          return response.json()
-        }).then(responseData => {
-          const loadedIngredients = [];
-          for (const key in responseData) {
-            loadedIngredients.push({
-              id: key,
-              title: responseData[key].title,
-              amount: responseData[key].amount
-            })
-          }
-          onLoadIngredients(loadedIngredients);
-        });
+        sendRequest(
+          'https://react-hooks-project-1acba-default-rtdb.firebaseio.com/ingredients.json' + query, 
+          'GET');
       }
+      
     },500);
     return () => { //cleanup function
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients, inputRef]); //no array is like component did update, with array, its like component did mount
+  }, [enteredFilter, inputRef, sendRequest]); //no array is like component did update, with array, its like component did mount
     
+  useEffect(() => {
+    if( !isLoading && !error &data ) {
+      const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        })
+      }
+      onLoadIngredients(loadedIngredients);
+    }
+  }, [data, isLoading, error, onLoadIngredients])
+
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input type="text"
             ref={inputRef}
             value={enteredFilter}
