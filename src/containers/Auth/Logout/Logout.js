85400a3769a13ecdec6a1674f08c1672af10bcd6
Delwin Best <mail@delwinbest.com>
2021-03-30 15:52:23 +0900

    logout convertext to func component

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import {Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions/index';
 
-class Logout extends Component {
+const Logout = props => {
+    useEffect(() => {
+        props.onLogout();
+        // eslint-disable-next-line
+    }, [])
 
-    componentDidMount() {
-        this.props.onLogout();
-    }
-    render () {
-        return <Redirect to="/"/>
-    }
+    return <Redirect to="/"/>
 }
 
 const mapDispatchToProps = (dispath) => {
